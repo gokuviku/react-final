17c1760a6e5a2a3ef69ad3aaa10b440f76a1dc2e
gokuviku <69633165+gokuviku@users.noreply.github.com>
2024-07-23 14:01:43 +0530

    completed

--- ans1/ans1/src/jsonApp.jsx
+++ ans1/ans1/src/jsonApp.jsx
@@ -1,5 +1,4 @@
 import { useParams } from 'react-router-dom';
-
 import db from '../db.json';
 
 const { name, email, password } = useParams();
@@ -10,4 +9,21 @@ if (!userData) {
   console.error('User not found');
 } else {
   console.log(userData);
+
+  // Update user data
+  const updatedUserData = { ...userData, favorites: [1, 3] }; // update favorites
+  const updatedDb = { ...db, users: db.users.map((user) => user.id === userData.id ? updatedUserData : user) };
+  console.log(updatedDb);
+
+  // Delete user data
+  const deletedDb = { ...db, users: db.users.filter((user) => user.id !== userData.id) };
+  console.log(deletedDb);
 }
+
+import fs from 'fs';
+
+// Update db.json file
+fs.writeFileSync('../db.json', JSON.stringify(updatedDb, null, 2));
+
+// Delete db.json file
+fs.writeFileSync('../db.json', JSON.stringify(deletedDb, null, 2));
